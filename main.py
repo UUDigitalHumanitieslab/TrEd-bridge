294a69eb0692546520bea6345b4f0356273d755a
Jelte van Boheemen <j.vanboheemen@gmail.com>
2018-10-05 09:55:13 +0200

    working editing, reparsing, saving of utterance

--- main.py
+++ main.py
@@ -1,36 +1,89 @@
-from tkinter import Tk, Label, Button, Text, StringVar, END, INSERT
-from tkinter.ttk import Label, Button, Entry
+import urllib.request
+# import xml.etree.ElementTree as ET
+import lxml.etree as ET
+from tkinter import END, INSERT, LEFT, Button, Label, StringVar, Text, Tk, X, Y, filedialog
+from tkinter.ttk import Button, Entry, Frame, Label, Style
+
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
 
 
-class MyFirstGUI:
-    def __init__(self, master):
+class UtteranceGUI:
+    def __init__(self, master, input_path):
+        s = Style()
+        s.theme_use('clam')
         self.master = master
-        master.title("A simple GUI")
+        master.title("Edit utterance")
 
-        self.label = Label(master, text="This is our first GUI!")
-        self.label.pack()
+        self.origutt, self.new_metadata = self.process_input(input_path)
+
+        self.origuttFrame = Frame(master, borderwidth=3)
+        self.origuttFrame.pack(fill=X, expand=True)
+        self.origuttVar = StringVar(
+            value="Original utterance:\n" + self.origutt)
+        self.origuttLabel = Label(
+            self.origuttFrame, textvariable=self.origuttVar)
+        self.origuttLabel.pack()
 
         self.labelText = StringVar()
         self.utterance = Entry(
             master, textvariable=self.labelText)
-        self.utterance.pack()
-        self.utterance.insert(END, "Dit is de originele uiting.")
+        self.utterance.pack(fill=X, expand=True)
+        self.utterance.insert(END, self.origutt)
 
         self.print_button = Button(master, text="print", command=self.printT)
-        self.print_button.pack()
+        self.print_button.pack(side=LEFT, fill='both', expand=True)
 
-        self.insert_button = Button(
-            master, text="insert", command=lambda: self.insert_into_entry("---"))
-        self.insert_button.pack()
-
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+        self.process_button = Button(
+            master, text="Process & Save", command=self.process_altered)
+        self.process_button.pack(side=LEFT, fill='both', expand=True)
 
         self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+        self.close_button.pack(side=LEFT, fill='both', expand=True)
 
-    def greet(self):
-        print("Greetings!")
+    def process_altered(self):
+        parsed = self.alpino_parse(self.utterance.get())
+        if parsed is None:
+            return
+        new_xml = self.construct_output_xml(parsed)
+        self.file_save(new_xml)
+
+    def construct_output_xml(self, parsed_string):
+        parsed_xml = ET.fromstring(parsed_string)
+        sentence = parsed_xml.find('sentence')
+        parsed_xml.insert(parsed_xml.index(sentence) +
+                          1, ET.XML(self.new_metadata))
+        origutt = parsed_xml.find('.//meta[@name="origutt"]')
+        origutt.attrib['value'] = self.utterance.get()
+        return ET.tostring(parsed_xml, encoding='utf-8')
+
+    def file_save(self, output_xml_string):
+        f = filedialog.asksaveasfile(mode='wb', defaultextension=".xml")
+        if f is None:
+            print('gaat fout')
+            return
+        f.write(output_xml_string)
+        f.close()
+
+    def alpino_parse(self, altered_utterance):
+        tokens = word_tokenize(altered_utterance)
+        url = 'http://gretel.hum.uu.nl/gretel4/api/src/router.php/parse_sentence/' +\
+            '%20'.join(tokens)
+        contents = urllib.request.urlopen(url).read()
+        return contents
+
+    def process_input(self, input_path):
+        tree = ET.parse(input_path)
+        root = tree.getroot()
+        metadata = root.find('metadata')
+        origutt = root.find('.//meta[@name="origutt"]')
+        origutt_old = '<meta type="text" name="origutt_old" value="{}"/>\n'.format(
+            origutt.get('value'))
+        metadata.insert(
+            metadata.index(origutt)+1, ET.XML(origutt_old))
+        new_metadata = ET.tostring(metadata) + b'\n'
+        return origutt.get('value'), new_metadata
 
     def printT(self):
         print(self.utterance.get())
@@ -44,5 +97,7 @@ class MyFirstGUI:
 
 
 root = Tk()
-my_gui = MyFirstGUI(root)
+root.geometry('500x500')
+my_gui = UtteranceGUI(
+    root, 'vk_example.xml')
 root.mainloop()
