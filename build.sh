69cca3df0ec881c3c8b32a8f261437b0056d5fdf
Jelte van Boheemen <j.vanboheemen@gmail.com>
2019-06-20 15:03:30 +0200

    few fixes

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-rm -r docs/extensions/package.xml ;
+rm -r docs/extensions/alpino_plus/package.xml ;
 rm -r docs/extensions/alpino_plus.zip ;
 rm -r docs/extensions/.DS_Store ;
-cp docs/src/package.xml docs/extensions ;
+cp docs/src/package.xml docs/extensions/alpino_plus/ ;
 cd docs/src; 
 zip -r ../extensions/alpino_plus.zip . -x "*.DS_Store"; 
 cd ../..;
