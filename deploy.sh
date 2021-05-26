c7f1d7b9366510712c3cc01e14c5fdc396953410
Jelte van Boheemen <j.vanboheemen@uu.nl>
2021-05-26 10:13:58 +0200

    Remove deploy file

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-echo "deleting old extensions from ssh home folder.." ;
-ssh -t im-dhstatic "sudo rm -r extensions;"
-echo "copying extensions to server.." ;
-scp -r dist/extensions im-dhstatic: ;
-echo "running server deploy script.." ;
-ssh -t im-dhstatic "sudo bash deploy_tred.sh";
-echo "done!"
--- deploy_tred.sh
+++
@@ -1,6 +0,0 @@
-cd /hum/web/dhstatic.hum.uu.nl/htdocs/tred ;
-rm -r extensions ;
-mkdir extensions ;
-cp -r /home/jvboheemen/extensions/. extensions ;
-chown -R www-data:www-data extensions ;
-chmod -R 777 extensions ;
